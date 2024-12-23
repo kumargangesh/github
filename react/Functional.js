90fca86cc5ef26b5f0411d47d95952c625b6ecd0
KumarGangesh <kumargangesh090@gmail.com>
2024-12-24 00:22:40 +0530

    more javascript folders added

---
+++ react/Functional.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function () {
+  return (
+    <div>
+        <center>
+            <h1>this is the Functional Component</h1>
+        </center>
+    </div>
+  )
+}
